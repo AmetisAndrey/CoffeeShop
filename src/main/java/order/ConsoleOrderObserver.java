286ef8cb770ed0d058e79271516747261cb1da78
AmetisAndrey <Ametisgoga70777@mail.ru>
2025-11-20 18:23:59 +0300

    new logics status order

--- src/main/java/order/ConsoleOrderObserver.java
+++ src/main/java/order/ConsoleOrderObserver.java
@@ -4,7 +4,42 @@ public class ConsoleOrderObserver implements OrderObserver {
 
     @Override
     public void onOrderStateChanged(Order order, OrderState newState) {
-        System.out.println("[NOTIFY] Order " + order.getId() +
-                " is now in state: " + newState.getName());
+
+        String emoji;
+        String state = newState.getName();
+
+        switch (state) {
+            case "NEW":
+            case "Заказ получен":
+                emoji = "🟣 Получен";
+                break;
+
+            case "PAID":
+            case "Заказ оплачен":
+                emoji = "💳 Оплачен";
+                break;
+
+            case "IN_PREPARATION":
+            case "Заказ готовится":
+                emoji = "🟡 Готовится";
+                break;
+
+            case "READY":
+            case "Заказ готов":
+                emoji = "🔵 Готов";
+                break;
+
+            case "COMPLETED":
+            case "Заказ завершён":
+                emoji = "🟢 Завершён";
+                break;
+
+            default:
+                emoji = state;
+        }
+
+        String shortId = order.getId().substring(0, 5) + "...";
+
+        System.out.println("🔔 Статус заказа №" + shortId + " → " + emoji);
     }
 }
